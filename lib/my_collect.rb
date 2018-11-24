7dce0393ac1351f0be0d290d863323caa10b4b4b
Steven Ott <stevenkott@icloud.com>
2018-11-24 19:49:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect(collection)
     i = 0 
     while i < collection.length 
-      array2 << yield(collection[i])
+      yield(collection[i])
       i += 1 
     end
-  return array2
     
   
 end
